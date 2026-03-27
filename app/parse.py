e3115291d8f014e942baf386a699b9072aef24ec
Vik <viktoria.om@gmail.com>
2026-03-27 14:18:10 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,12 @@
-from dataclasses import dataclass
+import requests
+import csv
+from dataclasses import dataclass, fields, astuple
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+from app.parse_authors import write_authors_to_csv, get_all_authors
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -8,8 +16,52 @@ class Quote:
     tags: list[str]
 
 
+QUOTE_FIELDS = [field.name for field in fields(Quote)]
+
+
+def parse_single_quote(quote: BeautifulSoup) -> Quote:
+    tags = quote.select_one(".keywords")["content"]
+    tags = [tag for tag in tags.split(",")] if tags else []
+
+    return Quote(
+        author=quote.select_one(".author").text,
+        text=quote.select_one(".text").text,
+        tags=tags
+    )
+
+
+def get_quotes_per_page(page_soup: BeautifulSoup) -> list[Quote]:
+    quotes = page_soup.select(".quote")
+    return [parse_single_quote(quote) for quote in quotes]
+
+
+def get_paginated_pages() -> list[Quote]:
+    response = requests.get(BASE_URL).content
+    page_soup = BeautifulSoup(response, "html.parser")
+    all_quotes = get_quotes_per_page(page_soup)
+    try:
+        pagination = page_soup.select_one("li.next a")["href"]
+        while pagination is not None:
+            new_url = urljoin(BASE_URL, pagination)
+            response = requests.get(new_url).content
+            new_page_soup = BeautifulSoup(response, "html.parser")
+            all_quotes.extend(get_quotes_per_page(new_page_soup))
+            pagination = new_page_soup.select_one("li.next a")["href"]
+    except TypeError:
+        pass
+    return all_quotes
+
+
+def write_quotes_to_csv(quotes: list[Quote], output_csv_path: str) -> None:
+    with open(output_csv_path, "w", encoding="utf-8", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(QUOTE_FIELDS)
+        writer.writerows([astuple(quote) for quote in quotes])
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    write_quotes_to_csv(get_paginated_pages(), output_csv_path)
+    write_authors_to_csv(get_all_authors())
 
 
 if __name__ == "__main__":
---
+++ app/parse_authors.py
@@ -0,0 +1,83 @@
+import requests
+import csv
+from dataclasses import dataclass, fields, astuple
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+
+BASE_URL = "https://quotes.toscrape.com/"
+
+
+@dataclass
+class Author:
+    name: str
+    bio: str
+
+
+AUTHOR_FIELDS = [field.name for field in fields(Author)]
+
+authors_cache = {}
+
+
+def get_one_author_info(author_url: str) -> Author:
+    existing_author = authors_cache.get(author_url)
+    if existing_author:
+        new_author = Author(**existing_author)
+    else:
+        new_url = urljoin(BASE_URL, author_url)
+        response = requests.get(new_url).content
+        new_page_soup = BeautifulSoup(response, "html.parser")
+        new_author = Author(
+            name=new_page_soup.select_one(".author-title").text,
+            bio=new_page_soup.select_one(".author-description").text.strip(),
+        )
+        authors_cache[author_url] = {
+            "name": new_author.name,
+            "bio": new_author.bio
+        }
+    return new_author
+
+
+def get_authors_urls_per_page(page_soup: BeautifulSoup) -> list:
+    quotes = page_soup.select(".quote")
+    authors_urls = [quote.select_one(" a")["href"] for quote in quotes]
+    return authors_urls
+
+
+def get_all_authors_urls() -> list:
+    response = requests.get(BASE_URL).content
+    page_soup = BeautifulSoup(response, "html.parser")
+    all_authors_urls = get_authors_urls_per_page(page_soup)
+
+    next_tag = page_soup.select_one("li.next a")
+    pagination = next_tag["href"] if next_tag else None
+
+    while pagination is not None:
+        new_url = urljoin(BASE_URL, pagination)
+        response = requests.get(new_url).content
+        new_page_soup = BeautifulSoup(response, "html.parser")
+        all_authors_urls.extend(get_authors_urls_per_page(new_page_soup))
+        next_tag = new_page_soup.select_one("li.next a")
+        pagination = next_tag["href"] if next_tag else None
+
+    return all_authors_urls
+
+
+def get_all_authors() -> list:
+    all_authors = []
+    all_authors_urls = get_all_authors_urls()
+    for author_url in all_authors_urls:
+        all_authors.append(get_one_author_info(author_url))
+    return all_authors
+
+
+def write_authors_to_csv(authors: list[Author]) -> None:
+    with open(
+            "authors_csv.csv",
+            "w",
+            encoding="utf-8",
+            newline=""
+    ) as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(AUTHOR_FIELDS)
+        writer.writerows([astuple(author) for author in authors])
